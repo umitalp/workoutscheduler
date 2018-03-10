915d0dcff49245a59cef2f282f050f2f968e529d
umitalp <umitalp@gmail.com>
2018-03-10 14:10:31 -0500

    ♻️Fix up styles and cleanup

--- App/Themes/ApplicationStyles.js
+++ App/Themes/ApplicationStyles.js
@@ -30,7 +30,7 @@ const ApplicationStyles = {
     sectionText: {
       ...Fonts.style.normal,
       paddingVertical: Metrics.doubleBaseMargin,
-      color: Colors.snow,
+      color: Colors.charcoal,
       marginVertical: Metrics.smallMargin,
       textAlign: 'center'
     },
